9af571f4b40985d302f0186f358f9ccde0df883a
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-07-16 20:12:48 +0200

    Migration file cleanup.

--- database/migrations/2022_07_15_140422_create_movies_table.php
+++ database/migrations/2022_07_15_140422_create_movies_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('imageUrl');
+            $table->string('genre');
             $table->timestamps();
         });
     }
