403c96a55b7d6f1fd5d39f5dfc7d1662b651e296
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-07-19 13:30:31 +0200

    Corrections.

--- database/migrations/2022_07_17_064549_create_favorites_table.php
+++ database/migrations/2022_07_17_064549_create_favorites_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->boolean('watched');
+            $table->boolean('watched')->default(false);;
 
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('movie_id');
